8b1f5d2ace0bd335d9ba14e5fe91a0f48296ac3a
Andrre Lima <andre.salatalima@gmail.com>
2022-11-23 21:04:22 +0100

    views, css, controllers and images

--- db/migrate/20221122203916_add_columns_to_product.rb
+++ db/migrate/20221122203916_add_columns_to_product.rb
@@ -1,9 +1,11 @@
 class AddColumnsToProduct < ActiveRecord::Migration[7.0]
   def change
-    add_column :products, :identifier, :string
+    add_column :products, :category, :string
     add_column :products, :name, :string
     add_column :products, :price, :float
     add_column :products, :image, :string
     add_column :products, :size, :string
+    add_column :products, :color, :string
+    add_column :products, :description, :string
   end
 end
