4e2f41ef0fc9161bf5cddfd71024b119c8754b74
Andrre Lima <andre.salatalima@gmail.com>
2022-11-23 21:04:22 +0100

    views, css, controllers and images

--- db/migrate/20221122203930_add_column_to_product.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToProduct < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :products, :identifier, :category
-    add_column :products, :color, :string
-    add_column :products, :description, :string
-    add_column :products, :category_id, :string
-  end
-end
