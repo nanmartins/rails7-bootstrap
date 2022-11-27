b858aacfc75bb110ec6f2f713b2726c277792056
Renan Martins <91997504+nanmartins@users.noreply.github.com>
2022-11-27 01:21:31 +0000

    improve js code of navlink to change the bg color of navlink based on body color

--- app/javascript/custom/navLink.js
+++ app/javascript/custom/navLink.js
@@ -1,12 +1,20 @@
 function initNavShow() {
   const divider = document.querySelector(".nav-divider");
   const nav = document.querySelector(".js-nav");
+  const targetEl = document.querySelector('.home-second-body');
+  const changeToRose = targetEl.getBoundingClientRect().top < 87;
   const isCloseToTop = divider.getBoundingClientRect().top < 0;
 
     if (isCloseToTop) {
       nav.classList.replace("js-navDontShow", "js-navShow");
     } else {
       nav.classList.replace("js-navShow", "js-navDontShow");
+    }
+
+    if (changeToRose) {
+      nav.classList.replace("js-navShow", "js-navRose");
+    } else {
+      nav.classList.replace("js-navRose", "js-navShow");
     }
 
 }
