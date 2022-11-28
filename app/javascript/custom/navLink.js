0a29bad044e38d7d7dc8623d0ebd69fc8ecbd022
Renan Martins <renanmartins989@gmail.com>
2022-11-28 15:11:54 +0000

    moduling view elements, basic footer sample, improve navlink js code. home base layout done, still missing queries, adjust positions and color but the basic is done

--- app/javascript/custom/navLink.js
+++ app/javascript/custom/navLink.js
@@ -2,8 +2,12 @@ function initNavShow() {
   const divider = document.querySelector(".nav-divider");
   const nav = document.querySelector(".js-nav");
   const targetEl = document.querySelector('.home-second-body');
+  const targetFooter = document.querySelector(".footer-medias-main-container");
+
   const changeToRose = targetEl.getBoundingClientRect().top < 87;
+  const backToWhite = targetFooter.getBoundingClientRect().top < 87;
   const isCloseToTop = divider.getBoundingClientRect().top < 0;
+
 
     if (isCloseToTop) {
       nav.classList.replace("js-navDontShow", "js-navShow");
@@ -15,6 +19,12 @@ function initNavShow() {
       nav.classList.replace("js-navShow", "js-navRose");
     } else {
       nav.classList.replace("js-navRose", "js-navShow");
+    }
+
+    if (backToWhite) {
+      nav.classList.replace("js-navRose", "js-navWhite");
+    } else {
+      nav.classList.replace("js-navWhite", "js-navRose");
     }
 
 }
